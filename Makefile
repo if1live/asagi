8c6bbff75f7e9facc68ee15f09e99e210131cc39
if1live <libsora25@gmail.com>
2018-03-04 13:55:59 +0900

    get/post sample

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	bash build.bat
--- echo/main.go
+++ echo/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -8,12 +11,63 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler() (Response, error) {
+type EchoResponse struct {
+	Blank   bool   `json:"blank"`
+	Message string `json:"message"`
+}
+
+func Handler_Blank() (Response, error) {
 	return Response{
 		Message: "Go Serverless v1.0! Your function executed successfully!",
 	}, nil
 }
 
+func makeResponse(status int, body interface{}) events.APIGatewayProxyResponse {
+	data, _ := json.Marshal(body)
+	return events.APIGatewayProxyResponse{
+		Body:       string(data),
+		StatusCode: status,
+	}
+}
+
+func Handler_APIGateway(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	switch request.HTTPMethod {
+	case "GET":
+		return handlerAPIGateway_GET(request)
+	case "POST":
+		return handlerAPIGateway_POST(request)
+	default:
+		return handlerAPIGateway_POST(request)
+	}
+}
+
+func handlerAPIGateway_GET(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var resp EchoResponse
+	msg, ok := request.QueryStringParameters["q"]
+	if ok {
+		resp = EchoResponse{
+			Message: msg,
+			Blank:   false,
+		}
+	} else {
+		resp = EchoResponse{
+			Message: "",
+			Blank:   true,
+		}
+	}
+
+	return makeResponse(200, resp), nil
+}
+
+func handlerAPIGateway_POST(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp := EchoResponse{
+		Message: request.Body,
+		Blank:   true,
+	}
+
+	return makeResponse(200, resp), nil
+}
+
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(Handler_APIGateway)
 }
